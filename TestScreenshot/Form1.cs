518ca232d9635b2b1d57f01d5666806dd2d1ada6
kjgriffin <kristopher.j.griffin@gmail.com>
2022-10-01 07:23:46 -0400

    centroid tracking

--- TestScreenshot/Form1.cs
+++ TestScreenshot/Form1.cs
@@ -14,6 +14,7 @@ namespace TestScreenshot
         public Form1()
         {
             InitializeComponent();
+            this.DoubleBuffered = true;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -61,6 +62,8 @@ namespace TestScreenshot
         CancellationTokenSource cts;
         Thread worker;
 
+        CentroidTracker _tracker = new CentroidTracker();
+
         private void button3_Click(object sender, EventArgs e)
         {
             cts?.Dispose();
@@ -70,8 +73,8 @@ namespace TestScreenshot
             classifier = new OpenCvSharp.CascadeClassifier("haarcascade_frontalface_default.xml");
 
 
-            //capture.Open(0, OpenCvSharp.VideoCaptureAPIs.ANY);
-            capture.Open(@"D:\Main\church\capture\2020-12-25 09-49-12.mp4");
+            capture.Open(0, OpenCvSharp.VideoCaptureAPIs.ANY);
+            //capture.Open(@"D:\Main\church\capture\2020-12-25 09-49-12.mp4");
             //capture.Open(@"D:\Main\church\capture\clip.mp4");
 
 
@@ -82,40 +85,55 @@ namespace TestScreenshot
         {
             while (!cts.IsCancellationRequested)
             {
+
+
                 var frameMat = capture.RetrieveMat();
+
+                //frameMat = frameMat.Blur(new OpenCvSharp.Size(16, 16));
+
                 var rects = classifier.DetectMultiScale(frameMat, 1.1, 5, OpenCvSharp.HaarDetectionTypes.ScaleImage, new OpenCvSharp.Size(30, 30));
-                //foreach (var rect in rects)
-                //{
-                //    Cv2.Rectangle(frameMat, rect, Scalar.Red);
-                //}
+
+                _tracker.UpdateTracks(rects);
 
                 Bitmap bmp = frameMat.ToBitmap();
-                Draw(bmp, rects);
+                Draw(bmp, _tracker.GetTracks());
 
                 bmp.Dispose();
                 frameMat.Dispose();
 
-                //Thread.Sleep(30);
+                Thread.Sleep(10);
             }
             capture.Release();
         }
 
-        private void Draw(Bitmap bmp, IEnumerable<Rect> rects)
+        private void Draw(Bitmap bmp, List<CentroidTrack> tracks)
         {
             if (InvokeRequired)
             {
-                Invoke(Draw, bmp, rects);
+                Invoke(Draw, bmp, tracks);
                 return;
             }
-            using (Graphics gfx = CreateGraphics())
+            using (Graphics g = CreateGraphics())
+            using (Bitmap b = new Bitmap(Size.Width, Size.Height))
+            using (Graphics gfx = Graphics.FromImage(b))
             {
+                gfx.Clear(Color.White);
                 gfx.DrawImage(bmp, 10, 10);
 
-                foreach (var rect in rects)
+                foreach (var track in tracks)
                 {
-                    gfx.DrawRectangle(Pens.Red, new System.Drawing.Rectangle(rect.X, rect.Y, rect.Width, rect.Height));
+                    gfx.DrawRectangle(track.Stale ? Pens.Red : Pens.Green, new System.Drawing.Rectangle(track.Centroid.Bounds.X, track.Centroid.Bounds.Y, track.Centroid.Bounds.Width, track.Centroid.Bounds.Height));
+                    gfx.FillRectangle(Brushes.Black, (int)track.Centroid.Center.X, (int)track.Centroid.Center.Y, 150, 25);
+                    gfx.DrawString(track.Name, DefaultFont, Brushes.White, new System.Drawing.Point((int)track.Centroid.Center.X, (int)track.Centroid.Center.Y));
                 }
 
+                for (int i = 0; i < tracks.Count; i++)
+                {
+                    gfx.FillRectangle(Brushes.White, 10, 23 + 20 * i, 150, 20);
+                    gfx.DrawString(tracks[i].Name, DefaultFont, Brushes.Black, new System.Drawing.Point(10, 20 + i * 20));
+                }
+
+                g.DrawImage(b, 0, 0);
             }
         }
 
